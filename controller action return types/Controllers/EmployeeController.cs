53f573cb67e7461c982c13d71a19397927b6dc58
1919saikumar1919 <124497384+1919saikumar1919@users.noreply.github.com>
2024-07-11 10:20:30 +0530

    Add project files.

---
+++ controller action return types/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using controller_action_return_types.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace controller_action_return_types.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public string Employee()
+        {
+            return "saikumar";
+        }
+        Employees all = new Employees();
+        [Route("get")]
+        [HttpGet]
+        public Employees AllEmployees()
+        {
+            all.name = "Test";
+            all.id = 1;
+      
+          
+           
+
+            return all;
+           
+        }
+    }
+}
---
+++ controller action return types/Models/Employees.cs
@@ -0,0 +1,10 @@
+﻿namespace controller_action_return_types.Models
+{
+    public class Employees
+    {
+        public int id { get; set; }
+        public string ? name { get; set; }
+        public string ? department { get; set; }  
+        public int salary { get; set; }
+    }
+}
